fd1e20aa5ae575fe9bf3f605985c771be56c1977
gabriel <1gabrielsilvarodrigues321rsg@gmail.com>
2026-06-09 15:56:07 -0300

    17- função que inicializa uma árvore vazia

--- arvore_generica.c
+++ arvore_generica.c
@@ -8,3 +8,19 @@ typedef struct No {
     struct No* esq;  
     struct No* dir;  
 } No;
+
+//função que inicializa uma árvore vazia 
+
+No* inicializar_arvore() {
+    return NULL; 
+}
+
+No* criar_no(int valor) {
+    No* novo = (No*)malloc(sizeof(No));
+    if (novo != NULL) {
+        novo->dado = valor;
+        novo->esq = NULL;
+        novo->dir = NULL;
+    }
+    return novo;
+}
